9dbd98954e5d2e98dbad905bf9710b6b3b3a6d15
olayinka <olayinka@obmondo.com>
2024-07-22 17:04:38 +0100

    updates
    Signed-off-by: olayinka <olayinka@obmondo.com>

--- include/info/info.h
+++ include/info/info.h
@@ -1,11 +1,23 @@
 #ifndef INFO_H
 #define INFO_H
 
-static int fs_checker(const char *full_path,
+#define _XOPEN_SOURCE 500
+
+#define DEFAULT_WALK 0
+#define MAX_PARALLEL_FD 20
+#define INFO_FMT "%-4s %-3d %-7lld   %-70s %s\n"
+#define INFO_FMT_STR "%-4s %-3s %-7s   %-70s %s\n"
+#define HEADER_COLOR "\x1b[1m\x1b[36m"
+#define DEFAULT_COLOR "\x1b[0m"
+
+#include <ftw.h>
+#include <sys/stat.h>
+
+const char *get_file_type(int file_type);
+
+int fs_checker(const char *full_path,
                     const struct stat *stat_buf,
                     int file_type,
-                    struct FTW *walk_buf);
-
-
+                    struct FTW *file_tree_walk_buf);
 
 #endif
--- src/info/info.c
+++ src/info/info.c
@@ -1,9 +1,32 @@
-#include "info.h"
+#define _XOPEN_SOURCE 500
+#include <ftw.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include "info/info.h"
 
-static int fs_checker(const char *full_path,
+const char *get_file_type(int file_type)
+{
+    switch(file_type) {
+        case FTW_D: return "d";
+        case FTW_DNR: return "dnr";
+        case FTW_DP: return "dp";
+        case FTW_F: return "f";
+        case FTW_NS: return "ns";
+        case FTW_SL: return "sl";
+        case FTW_SLN: return "sln";
+    }
+    return "???";
+}
+
+int fs_checker(const char *full_path,
                     const struct stat *stat_buf,
                     int file_type,
-                    struct FTW *walk_buf);
+                    struct FTW *file_tree_walk_buf)
 {
-
+    printf(INFO_FMT, get_file_type(file_type),
+        file_tree_walk_buf->level,
+        (long long)stat_buf->st_size,
+        full_path, 
+        full_path + file_tree_walk_buf->base);
+    return 0;
 }
