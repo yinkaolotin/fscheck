b9a4be5d74c1c0364d98d4510ac19df8a2476012
olayinka <olayinka@obmondo.com>
2024-07-22 13:50:51 +0100

    update macros
    Signed-off-by: olayinka <olayinka@obmondo.com>

--- include/exception/exception.h
+++ include/exception/exception.h
@@ -1,19 +1,23 @@
 #ifndef EXCEPTION_EXCEPTION_H
 #define EXCEPTION_EXCEPTION_H
 
+
 #define USAGE_EXCEPTION "Usage: %s <starting-directory>\n"
-#define PARSE_EXCEPTION "failed to parse commmand arguments"
+#define PARSE_EXCEPTION "failed to parse commmand arguments\n"
 #define FS_EXCEPTION "Cannot check whole file system " \
     "please specify an inner directory instead e.g " \
     "/home, /home/Documents \n"
 
 #define EXCEPTION_AND_EXIT(fmt, ...) \
-    fprintf(stderr, fmt, __VA_ARGS__); \
-    exit(EXIT_FAILURE);
+    do { \
+        fprintf(stderr, fmt, ##__VA_ARGS__); \
+        exit(EXIT_FAILURE); \
+    } while (0)
 
 #define EXCEPTION_AND_RETURN(value, fmt, ...) \
-    fprintf(stderr, fmt, __VA_ARGS__); \
-    return value;
-
+    do { \
+        fprintf(stderr, fmt, ##__VA_ARGS__); \
+        return value; \
+    } while (0)
 
 #endif
