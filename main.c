4b3be1bfe3ce1cc62a8432b0a71d7706481dec10
olayinka <olayinka@obmondo.com>
2024-07-24 17:03:56 +0100

    updates
    Signed-off-by: olayinka <olayinka@obmondo.com>

--- main.c
+++ main.c
@@ -8,9 +8,10 @@ int main(int argc, char *argv[])
 {
     const struct cmd *c = parse_cmd(argc, argv);
     printf(INFO_FMT_STR, "TYPE", "LVL", "SIZE", "FULL PATH", "BASE");
-    if (nftw(argv[1], fs_checker, MAX_PARALLEL_FD, DEFAULT_WALK) == -1) {
+    if (nftw(argv[1], scrape_file_meta, MAX_PARALLEL_FD, DEFAULT_WALK) == -1) {
         EXCEPTION_AND_EXIT("nftw");
     }
+    print_file_meta(c);
     free(c->directory);
     free((void *)c);
     exit(EXIT_SUCCESS);
