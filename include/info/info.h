df42ad24f9cd1bc60ba437578692d0ab633257fd
olayinka <olayinka@obmondo.com>
2024-07-24 17:03:45 +0100

    display aggregate info by flag
    Signed-off-by: olayinka <olayinka@obmondo.com>

--- include/info/info.h
+++ include/info/info.h
@@ -5,19 +5,50 @@
 
 #define DEFAULT_WALK 0
 #define MAX_PARALLEL_FD 20
-#define INFO_FMT "%-4s %-3d %-7lld   %-70s %s\n"
+
+#define DEFAULT_INFO_FMT "%-4s %-3d %-7lld   %-70s %s\n"
+#define AGGREGATE_INFO_FMT "%o %s (%d) %s (%d) %s %s %s\n"
 #define INFO_FMT_STR "%-4s %-3s %-7s   %-70s %s\n"
 #define HEADER_COLOR "\x1b[1m\x1b[36m"
 #define DEFAULT_COLOR "\x1b[0m"
 
+#define MAX_PATH_LENGTH 100
+#define MAX_FILES_READABLE 10000
+
 #include <ftw.h>
+#include <time.h>
 #include <sys/stat.h>
+
+typedef struct {
+    char path[MAX_PATH_LENGTH];
+    const char *type;
+    off_t size;
+
+    // default only
+    int tree_lvl;
+    int file_name_idx;
+
+    mode_t permissions;
+    uid_t owner_id;
+    gid_t group_id;
+    char owner[256];
+    char group[256];
+    time_t accessed_time;
+    time_t modified_time;
+    time_t changed_time;
+} FileMeta;
 
 const char *get_file_type(int file_type);
 
-int fs_checker(const char *full_path,
+int scrape_file_meta(const char *full_path,
                     const struct stat *stat_buf,
                     int file_type,
-                    struct FTW *file_tree_walk_buf);
+                    struct FTW *file_tree_buf);
+
+void print_default_mode();
+
+void print_aggregate_mode();
+
+void print_file_meta();
 
 #endif
--- src/info/info.c
+++ src/info/info.c
@@ -2,7 +2,13 @@
 #include <ftw.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <string.h>
+#include "parser/parser.h"
 #include "info/info.h"
+#include "exception/exception.h"
+
+static int file_count = 0;
+static FileMeta file_meta_storage[MAX_FILES_READABLE];
 
 const char *get_file_type(int file_type)
 {
@@ -18,15 +24,71 @@ const char *get_file_type(int file_type)
     return "???";
 }
 
-int fs_checker(const char *full_path,
+int scrape_file_meta(const char *full_path,
                     const struct stat *stat_buf,
                     int file_type,
-                    struct FTW *file_tree_walk_buf)
+                    struct FTW *file_tree_buf)
 {
-    printf(INFO_FMT, get_file_type(file_type),
-        file_tree_walk_buf->level,
-        (long long)stat_buf->st_size,
-        full_path, 
-        full_path + file_tree_walk_buf->base);
+    if (file_count >= MAX_FILES_READABLE) {
+        EXCEPTION_AND_RETURN(1, "Maximum file limit reached\n");
+    }
+
+    FileMeta *fm = &file_meta_storage[file_count];
+    file_count++;
+
+    strncpy(fm->path, full_path, MAX_PATH_LENGTH - 1);
+    fm->path[MAX_PATH_LENGTH - 1] = '\0';
+
+    fm->type = get_file_type(file_type);
+    fm->size = stat_buf->st_size;
+
+    fm->tree_lvl = file_tree_buf->level;
+    fm->file_name_idx = file_tree_buf->base;
+
+    fm->permissions = stat_buf->st_mode & 0777;
+    fm->owner_id = stat_buf->st_uid;
+    fm->group_id = stat_buf->st_gid;
+    fm->accessed_time = stat_buf->st_atime;
+    fm->modified_time = stat_buf->st_mtime;
+    fm->changed_time = stat_buf->st_ctime;
+
     return 0;
+}
+
+void print_default_mode()
+{
+    for (int i = 0; i < file_count; i++) {
+        FileMeta *fm = &file_meta_storage[i];
+        printf(DEFAULT_INFO_FMT,
+            fm->type,
+            fm->tree_lvl,
+            (long long)fm->size,
+            fm->path, 
+            fm->path + fm->file_name_idx);
+    }
+}
+
+void print_aggregate_mode()
+{
+    for (int i = 0; i < file_count; i++) {
+        FileMeta *fm = &file_meta_storage[i];
+        printf(AGGREGATE_INFO_FMT,
+            fm->permissions,
+            fm->owner,
+            fm->owner_id,
+            fm->group,
+            fm->group_id,
+            ctime(&fm->accessed_time),
+            ctime(&fm->modified_time),
+            ctime(&fm->changed_time));
+    }
+}
+
+void print_file_meta(struct cmd *c)
+{
+    if (c->flags & FLAG_AGGREGATE_INFO) {
+        print_aggregate_mode();
+        return;
+    }
+    print_default_mode();
 }
