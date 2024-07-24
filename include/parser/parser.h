4a3615256a2ee8c615dea53cc16cf6d47d8e5cce
olayinka <olayinka@obmondo.com>
2024-07-24 17:03:17 +0100

    updates parsing
    Signed-off-by: olayinka <olayinka@obmondo.com>

--- include/parser/parser.h
+++ include/parser/parser.h
@@ -1,8 +1,16 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#define EXPECTED_OPTS_FMT "a"
+#define OPT_AGGREGATE 'a'
+#define OPTS_UNKNOWN '?'
+
+#define FLAG_DEFAULT_INFO 0x01
+#define FLAG_AGGREGATE_INFO 0x02
+
 struct cmd {
     char *directory;
+    unsigned int flags;
 };
 
 const struct cmd *parse_cmd(int argc, char *argv[]);
